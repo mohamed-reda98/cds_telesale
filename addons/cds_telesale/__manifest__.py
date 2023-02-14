65f8413aca48acad0460602e1de2fdd74b6dbf8c
mohamedreda98 <mohamed_reda741@gmail.com>
2023-02-14 14:11:12 +0200

    initial commit

---
+++ addons/cds_telesale/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Telesales",
+    'summary': """ Telesales Module
+    """,
+    'description': """
+    Telesales for orders and Quotations and Sale Orders
+    """,
+    'author': "CDS Solutions SRL",
+    'website': "www.cdsegypt.com",
+    'contributors': [
+        'Ramadan Khalil <rkhalil1990@gmail.com>',
+        'Mohamed Reda <mohamed_reda741@gmail.com>',
+    ],
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    "pre_init_hook": None,
+    "post_init_hook": None,
+}
---
+++ addons/cds_telesale/models/model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#    Copyright (c) 2021 CODOOS SRL. (http://codoos.com)
+#    Maintainer: Eng.Ramadan Khalil (<rkhalil1990@gmail.com>)
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+##############################################################################
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError, UserError
+from datetime import datetime, date
