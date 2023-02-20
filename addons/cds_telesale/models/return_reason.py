f3d50e88b30d23fe8a6e8a37bac485270dd1aff1
mohamedreda98 <mohamed_reda741@gmail.com>
2023-02-20 13:33:17 +0200

    solve return reason problem

--- addons/cds_telesale/models/return_reason.py
+++ addons/cds_telesale/models/return_reason.py
@@ -7,3 +7,10 @@ class ReturnReason(models.Model):
     _description = 'Return Reason'
 
     name = fields.Char(string='Return Reason')
+
+
+
+    # def change_record_reason(self):
+    #     # Add code here
+    #     for record in records:
+    #         record
--- addons/cds_telesale/models/telesale.py
+++ addons/cds_telesale/models/telesale.py
@@ -5,4 +5,5 @@ from odoo import fields, models
 class Telesale(models.Model):
     _inherit = 'sale.order'
 
+    reason = fields.Many2one(comodel_name="return.reason", string="Return Reason" )
     is_tele_sale = fields.Boolean(string="TeleSale", default=False)
