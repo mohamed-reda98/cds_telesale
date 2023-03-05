f5079ba3deddf5cd366be480969d2b8004b9cf6c
mohamedreda98 <mohamed_reda741@gmail.com>
2023-03-05 12:56:11 +0200

    report excel but not finished

--- addons/cds_telesale/report/sale_order_line_excel.py
+++ addons/cds_telesale/report/sale_order_line_excel.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import base64
+import io
+
 from odoo import models
 
 
@@ -8,11 +11,41 @@ class SaleOrderXlsx(models.AbstractModel):
     _description = 'Get Sale Order Lines as Excel.'
 
     def generate_xlsx_report(self, workbook, data, products):
+        products = self.env['sale.order'].search([])
+        # sale_orders = self.env['sale.order'].browse(products)
+        sale_order_lines = products.mapped('order_line')
+        product_ids = sale_order_lines.mapped('product_id')
         print("Hi From Excel Class")
-        print(data, products)
-        for obj in products:
-            report_name = obj.name
+        print(data, product_ids)
+        row = 1
+        bold = workbook.add_format({'bold': True})
+        sheet = workbook.add_worksheet()
+        sheet.write(0, 0, 'Product Image', bold)
+        sheet.write(0, 1, 'Product Name', bold)
+        sheet.write(0, 2, 'Quantity', bold)
+        sheet.write(0, 3, 'Total', bold)
+
+        for obj in product_ids:
+            # report_name = obj.name
             # One sheet by partner
-            sheet = workbook.add_worksheet(report_name[:31])
-            bold = workbook.add_format({'bold': True})
-            sheet.write(0, 0, obj.name, bold)
+            product_lines = sale_order_lines.filtered(lambda l: l.product_id == obj)
+            pr_qty = sum([l.product_uom_qty for l in product_lines])
+            pr_amount = sum([l.price_subtotal for l in product_lines])
+            pr_data = {
+                'product': obj,
+                'qty': pr_qty,
+                'amount': pr_amount
+            }
+            if obj.image_1920:
+                product_image = io.BytesIO(base64.b64decode(obj.image_128))
+                sheet.insert_image(row, 0, "image.png", {'image_data': product_image}, )
+
+            # sheet.write(row, 0, pr_data['product'].display_name)
+            sheet.write(row, 1, pr_data['product'].display_name)
+            # sheet.write(row, 1, pr_data['qty'])
+            sheet.write(row, 2, pr_data['qty'])
+            sheet.write(row, 3, pr_data['amount'])
+            row += 1
+
+            # sheet.write(0, 0, obj.name, bold)
+            # sheet.write(0, 0, obj.user_id, bold)
