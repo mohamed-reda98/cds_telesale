52bd37218f60f9195a771976ee0d88f809bb91b1
mohamedreda98 <mohamed_reda741@gmail.com>
2023-03-06 16:50:06 +0200

    work well but totals

--- addons/cds_telesale/report/sale_order_line_excel.py
+++ addons/cds_telesale/report/sale_order_line_excel.py
@@ -11,8 +11,9 @@ class SaleOrderXlsx(models.AbstractModel):
     _description = 'Get Sale Order Lines as Excel.'
 
     def generate_xlsx_report(self, workbook, data, products):
-        products = self.env['sale.order'].search([])
+        # products = self.env['sale.order'].search([])
         # sale_orders = self.env['sale.order'].browse(products)
+        sale_orders = self.env['sale.order'].browse(products)
         sale_order_lines = products.mapped('order_line')
         product_ids = sale_order_lines.mapped('product_id')
         print("Hi From Excel Class")
@@ -25,7 +26,7 @@ class SaleOrderXlsx(models.AbstractModel):
         sheet.write(0, 1, 'Product Name', bold)
         sheet.write(0, 2, 'Quantity', bold)
         sheet.write(0, 3, 'Total', bold)
-
+        total_sum = []
         for obj in product_ids:
             # report_name = obj.name
             # One sheet by partner
@@ -35,7 +36,7 @@ class SaleOrderXlsx(models.AbstractModel):
             pr_data = {
                 'product': obj,
                 'qty': pr_qty,
-                'amount': pr_amount
+                'amount': pr_amount,
             }
 
             if obj.image_128:
@@ -49,7 +50,13 @@ class SaleOrderXlsx(models.AbstractModel):
             sheet.write(row, 3, pr_data['amount'])
             row += 1
 
+        sheet.write(row, 3, 'total')
+        # sheet.write(row, 4, total_sum)
+
         # print(data, product_ids.name)
 
         # sheet.write(0, 0, obj.name, bold)
         # sheet.write(0, 0, obj.user_id, bold)
+        # sale_line_ids_per_move = defaultdict(lambda: self.env['sale.order.line'])
+        # for move_line in move_line_read_group:
+        #     sale_line_ids_per_move[move_line['move_id'][0]] += self.env['sale.order.line'].browse(move_line['sale_line_ids'])
